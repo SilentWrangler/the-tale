ce0b3d0076e0addef41866099847e7ddb174cc3a
SilentWrangler <thousandgoblins@gmail.com>
2020-05-26 18:48:18 +0300

    Лечим жертв копипасты

--- src/the_tale/the_tale/settings.py
+++ src/the_tale/the_tale/settings.py
@@ -26,7 +26,7 @@ SENTRY_RAVEN_CONFIG = None
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "NAME": os.path.join(PROJECT_DIR, "db.sqlite3")
     }
 }
 
